0e53a646bcda1dd435bb618b628256b35dc8118a
Nockiro <info@nockiro.de>
2018-06-09 20:11:18 +0200

    # fixed mobile design (see #2)

--- OnlineResxTranslator/App_Code/XMLFile.cs
+++ OnlineResxTranslator/App_Code/XMLFile.cs
@@ -16,7 +16,8 @@ public class XMLFile
     // set the not checked items
     public static readonly string[] NotArgs = new string[] { "Icon", "Size", "ImageStream", "Image", "Width", "Location", "ImeMode", "TabIndex", "TextAlign",
                                 "ToolTip", "Dock", "ClientSize", "Enabled", "Visible", "Groups", "ThousandsSeparator", "AutoSize", "BackgroundImage", "Type", "ZOrder", "Parent", "Name",
-                                "Padding", "Anchor", "AutoScaleDimensions", "Multiline", "Font", "TextImageRelation", "SplitterDistance","FlatStyle", "ColumnCount", "RowCount", "LayoutSettings", "CheckAlign"};
+                                "Padding", "Anchor", "AutoScaleDimensions", "Multiline", "Font", "TextImageRelation", "SplitterDistance","FlatStyle", "ColumnCount", "RowCount", "LayoutSettings", "CheckAlign",
+                                "Item"};
 
     public XMLFile()
     {
